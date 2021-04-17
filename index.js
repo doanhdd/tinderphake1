b9c47e5d019454c1973d1d0c4d1a243b41f8f814
doanh <71993951+doanhdd@users.noreply.github.com>
2021-04-17 14:55:17 +0700

    Initial commit

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 var express = require('express');
 var expressHbs = require('express-handlebars')
 var app = express();
-app.listen(process.env.PORT || '3001');
+app.listen(process.env.PORT || '3001', function () {
+    console.log('doanh ')
+});
+// getting-started.js
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://doanh:doanh@doanhcluster.arwi8.mongodb.net/TinderFake?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    // we're connected!
+    console.log("vào ")
+});
+
+var user= new mongoose.Schema({
+    email:String,
+    pass:String,
+    username:String,
+    avatar:String
+})
+var userInsert = db.model('ListUser',user);
+
+
+
 app.engine('handlebars', expressHbs({
     layoutsDir: __dirname + '/views/layouts',
-    // layout cha mặc định
+    // layout cha mặc địnhco
     defaultLayout: 'main'
 }));
 app.use(express.static('assets'));
@@ -16,8 +38,21 @@ app.get('/', function (request,
 })
 
 app.get('/dangki.handlebars', function (req, res) {
-    res.render("dangki", {layout: 'main'});
+
+    var action=userInsert({
+        email:"String1",
+        pass:"Stri1ng",
+        username:"Strin1g",
+        avatar:"Strin1g"
+    }).save(function (error){
+        if(error){
+            res.render('dangki', { layout: 'main' });
+        }else {
+            res.render("dangki", {layout: 'main'});
+        }
+    });
 });
+
 
 app.get('/login.handlebars', function (req, res) {
     res.render("login", {layout: 'main',});
